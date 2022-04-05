9be8fac1af64caa86537542c56077f609b483949
Sudip Bhattarai <sudipbhattarai100@gmail.com>
2022-04-05 11:00:38 +0545

    Update response type for server

--- server/Main.hs
+++ server/Main.hs
@@ -4,10 +4,11 @@ module Main where
 -- import Cardano.Contrib.Kubær.ChainInfo (IsNetworkCtx (toFullNetworkContext, toNetworkContext), getDefaultTestnetContext, readContextFromEnv)
 import Network.Wai.Handler.Warp (run)
 import Server (app)
+import Cardano.Contrib.Kubær.ChainInfo
 
 main = do
-  -- ctx <- readContextFromEnv
-  -- networkCtx <- toNetworkContext ctx
+  ctx <- readContextFromEnv
+  chainInfo <- withDetails ctx
   let port=8081
   putStrLn $ "Starting server on port " ++ show port ++"..."
-  run port app
+  run port $ app chainInfo
--- test/Main.hs
+++ test/Main.hs
@@ -1,40 +1,41 @@
--- import Cardano.Api (ExecutionUnits (ExecutionUnits), ScriptData (ScriptDataNumber))
--- import Cardano.Contrib.Easy.Context
--- import Cardano.Contrib.Easy.Error
--- import Cardano.Contrib.Easy.Parsers (createMintingScriptWitness, parseAnyScript)
--- import Cardano.Contrib.Easy.Util (queryUtxos)
--- import Data.Text as T
+import Cardano.Api (ExecutionUnits (ExecutionUnits), ScriptData (ScriptDataNumber))
+import Cardano.Contrib.Kubær.ChainInfo
+import Cardano.Contrib.Kubær.Error
+import Cardano.Contrib.Kubær.Parsers 
+import Cardano.Contrib.Kubær.Util 
+import Data.Text as T
 
 -- -- getAddrAnyFromEra addrEra = unMaybe (SomeError "unexpected error converting address to another type") (deserialiseAddress AsAddressAny (serialiseAddress  addrEra))
 
 main = do
---   -- ps <- parseAnyScript $ T.pack "{\
---   -- \\"type\": \"PlutusScriptV1\",\
---   -- \\"description\": \"\",\
---   -- \\"cborHex\": \"4e4d01000033222220051200120011\"\
---   -- \}"
---   -- pscriptWitness <- createTxInScriptWitness AlonzoEra ps
---   -- print $ show pscriptWitness
+  -- ps <- parseAnyScript $ T.pack "{\
+  -- \\"type\": \"PlutusScriptV1\",\
+  -- \\"description\": \"\",\
+  -- \\"cborHex\": \"4e4d01000033222220051200120011\"\
+  -- \}"
+  -- pscriptWitness <- createTxInScriptWitness AlonzoEra ps
+  -- print $ show pscriptWitness
 
---   let requiredMemory = 700000000
---       requiredSteps = 700000000
+  let requiredMemory = 700000000
+      requiredSteps = 700000000
 
---   ss <- parseAnyScript $ T.pack "{\"type\":\"all\",\"scripts\":[{\"keyHash\":\"ecfe3f453fe6d6048d53095c931f59e02bb504968b7beff3f8c010de\",\"type\":\"sig\"}]}"
+  ss <- parseAnyScript $ T.pack "{\"type\":\"all\",\"scripts\":[{\"keyHash\":\"ecfe3f453fe6d6048d53095c931f59e02bb504968b7beff3f8c010de\",\"type\":\"sig\"}]}"
 
---   sscriptWitness <- createMintingScriptWitness ss (ScriptDataNumber 1) (ExecutionUnits requiredMemory requiredSteps)
+  sscriptWitness <- createMintingScriptWitness ss (ScriptDataNumber 1) (ExecutionUnits requiredMemory requiredSteps)
 
---   print $ show sscriptWitness
+  print $ show sscriptWitness
 
---   -- minCtx <- getDefaultTestnetContext
---   -- ctx <- toNetworkContext minCtx
---   -- let rawAddr = "addr_test1vrk0u0698lndvpyd2vy4eyclt8szhdgyj69hhmlnlrqpphssqgntw"
---   -- addrInEra <- case deserialiseAddress (AsAddressInEra AsAlonzoEra) (T.pack rawAddr) of
---   --     Nothing -> fail "Cannot deserialise Address"
---   --     Just aie -> return aie
+  minCtx <- getDefaultTestnetContext
+  ctx <- withDetails  minCtx
+  
+  let rawAddr = "addr_test1vrk0u0698lndvpyd2vy4eyclt8szhdgyj69hhmlnlrqpphssqgntw"
+  addrInEra <- case deserialiseAddress (AsAddressInEra AsAlonzoEra) (T.pack rawAddr) of
+      Nothing -> fail "Cannot deserialise Address"
+      Just aie -> return aie
 
---   -- addrAny <- getAddrAnyFromEra addrInEra
+  addrAny <- getAddrAnyFromEra addrInEra
 
---   -- utxos <- queryUtxos (networkCtxConn ctx) addrAny
---   -- putStrLn $ toConsoleText  "  "  utxos
+  utxos <- queryUtxos (networkCtxConn ctx) addrAny
+  putStrLn $ toConsoleText  "  "  utxos
 
   print "ok"
