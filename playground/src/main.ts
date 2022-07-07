bca059b900259a04f9416b97148463691545b089
nabin6246 <nabin62246@gmail.com>
2022-07-07 14:44:54 +0545

    Add vue toast plugin

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -3,6 +3,8 @@ import router from "./router"
 import App from './App.vue'
 import './assets/base.css';
 import '@dafcoe/vue-notification/dist/vue-notification.css'
+import 'vue-toast-notification/dist/theme-sugar.css';
+import ToastPlugin from 'vue-toast-notification';
 
 
 // @ts-ignore
@@ -10,4 +12,5 @@ import VueNotificationList from '@dafcoe/vue-notification'
 const app=createApp(App)
 app.use(VueNotificationList)
 app.use(router)
+app.use(ToastPlugin)
 app.mount('#app')
