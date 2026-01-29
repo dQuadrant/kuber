a3f1c16793bd20bc1373056826e4da8825052b1a
kushal <csit22081006_kushal@achsnepal.edu.np>
2026-01-29 13:22:53 +0545

    Fix: fixed the .sk and .skey permissions

--- kuber-hydra/devnet/setup-devnet.sh
+++ kuber-hydra/devnet/setup-devnet.sh
@@ -19,7 +19,16 @@ fi
 sed -i.bak "s/\"startTime\": [0-9]*/\"startTime\": $(date +%s)/" "cardano-node/genesis-byron.json" && \
 sed -i.bak "s/\"systemStart\": \".*\"/\"systemStart\": \"$(date -u +%FT%TZ)\"/" "cardano-node/genesis-shelley.json"
 
+chmod 600 cardano-node/faucet.sk
+chmod 600 cardano-node/faucet.vk
+chmod 600 cardano-node/kes.skey
+chmod 600 cardano-node/vrf.skey
 
-
+if [ ! -f .env ]; then
+    cat > .env << 'EOF'
+HYDRA_SCRIPTS_TX_ID=
+EOF
+    
+fi
 echo "Prepared devnet, you can start the cluster now"
 
